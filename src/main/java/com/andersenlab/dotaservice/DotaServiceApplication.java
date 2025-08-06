cce7baae21be0627eb812351d47d1c3ae9414ca2
Dmytro Titov <octopusless@gmail.com>
2025-08-06 21:01:23 +0300

    Initial commit

---
+++ src/main/java/com/andersenlab/dotaservice/DotaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.andersenlab.dotaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotaServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DotaServiceApplication.class, args);
+  }
+
+}
